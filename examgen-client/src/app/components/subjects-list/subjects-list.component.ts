51f5a13024bee22abe1aae92d6fb5cac006709b1
LightDestory <apb231@gmail.com>
2021-08-01 18:08:58 +0200

    forgot files

--- examgen-client/src/app/components/subjects-list/subjects-list.component.ts
+++ examgen-client/src/app/components/subjects-list/subjects-list.component.ts
@@ -1,32 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
 import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { Title } from '@angular/platform-browser';
 import { Subject } from 'src/app/models/subject';
 import { EndpointSharedService } from 'src/app/services/endpoint/shared/endpoint-shared.service';
 import { EndpointSubjectsService } from 'src/app/services/endpoint/subjects/endpoint-subjects.service';
 import { SpinnerLoadingComponent } from '../dialogs/spinner-loading/spinner-loading.component';
+import {MatPaginator} from '@angular/material/paginator';
+import {MatTableDataSource} from '@angular/material/table';
 
 @Component({
   selector: 'app-subjects-list',
   templateUrl: './subjects-list.component.html',
   styleUrls: ['./subjects-list.component.scss']
 })
-export class SubjectsListComponent implements OnInit {
 
-  subjects: Subject[];
+export class SubjectsListComponent implements OnInit, AfterViewInit  {
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
   private loadingSpinnerRef: MatDialogRef<SpinnerLoadingComponent>|null = null;
+  private subjects: Subject[] = [];
+  displayedColumns: string[] = ['_id', 'count', 'rename', 'delete'];
+  dataSource: MatTableDataSource<Subject> = new MatTableDataSource<Subject>();
 
   constructor(
     private pageTitle: Title,
     private endpoint: EndpointSubjectsService,
     private matdialog: MatDialog,
-    private helper: EndpointSharedService) {
-    this.subjects = [];
-   }
+    private helper: EndpointSharedService) {}
 
   ngOnInit(): void {
     this.pageTitle.setTitle("ExamGen - Subjects Listing");
     this.loadingSpinnerRef = this.helper.openLoadingDialog();
+  }
+
+  ngAfterViewInit() {
     this.getData();
   }
 
@@ -35,12 +41,18 @@ export class SubjectsListComponent implements OnInit {
       data => {
         this.loadingSpinnerRef!.close();
         this.subjects = (<Subject[]>data.result);
+        this.dataSource.data.push(...this.subjects);
+        this.dataSource.paginator = this.paginator;
       },
       error => {
         this.loadingSpinnerRef!.close();
         this.helper.showServiceErrorDialog(error.status);
       }
     )
+  }
+
+  public renameSubject(subjectName: string): void {
+    alert(subjectName);
   }
 
 }
